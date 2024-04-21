bdd7969c1a4cbade80d56b273eb363f7de6ec3c5
Rasel Ahmmed <raselahmmed@hotmail.com>
2024-04-21 09:33:05 +0600

    2024-04-21

--- SimplCommerce/src/SimplCommerce.WebHost/IdentityServer/IdentityServerConfig.cs
+++ SimplCommerce/src/SimplCommerce.WebHost/IdentityServer/IdentityServerConfig.cs
@@ -16,7 +16,7 @@ namespace SimplCommerce.WebHost.IdentityServer
         public static IEnumerable<ApiResource> Apis =>
             new List<ApiResource>
             {
-                new ApiResource("api.simplcommerce", "Rapid System API")
+                new ApiResource("api.simplcommerce", "Simpl Commerce API")
             };
 
         public static IEnumerable<Client> Clients =>
